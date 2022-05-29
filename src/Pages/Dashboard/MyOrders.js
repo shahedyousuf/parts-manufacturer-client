5c2dfa39b5f4e96104321b338af66c6fea7cb6eb
shahedyousuf <smshahed.yousuf@gmail.com>
2022-05-29 08:49:30 +0200

    my orders page

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyOrders = () => {
+    const [orders, setOrders] = useState([]);
+    const [user] = useAuthState(auth);
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/order?userEmail=${user.email}`)
+                .then(res => res.json())
+                .then(data => setOrders(data));
+        }
+    }, [user])
+
     return (
         <div>
-            <h2>My Orders</h2>
+            <h2>My Orders: {orders.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Name</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map((order, index) => <tr>
+                                <th>{index + 1}</th>
+                                <td>{order.orderName}</td>
+                                <td>{order.quantity}</td>
+                                <td>{order.price}</td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
